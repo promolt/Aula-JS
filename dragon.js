31bab45950b696466c57aaf5d88512fda5143401
promolt <oi.phcarvalho@gmail.com>
2016-08-29 22:16:54 -0300

    Primeiro commit

---
+++ dragon.js
@@ -0,0 +1,23 @@
+var slaying = true;
+// Um pouco de mágica matemática nova para calcular as chances
+// de atingir o dragão. Vamos falar disso logo!
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
+var totalDamage = 0;
+
+while (slaying) {
+  if (youHit) {
+    console.log("Você atingiu o dragão e causou " + damageThisRound + " de dano!");
+    totalDamage += damageThisRound;
+    
+    if (totalDamage >= 4) {
+      console.log("Você conseguiu! Voce matou o dragao!");
+      slaying = false;
+    } else {
+      youHit = Math.floor(Math.random() * 2);
+    }
+  } else {
+    console.log("Você foi derrotado pelo dragão! Ficou torrado.");
+    slaying = false;
+  }
+}
