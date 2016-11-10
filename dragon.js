75f55656cfcd9cc0b91085c6ecc966c3247d30a3
Pedro Carvalho <oi.phcarvalho@gmail.com>
2016-11-10 12:56:58 -0200

    Update dragon.js

--- dragon.js
+++ dragon.js
@@ -1,23 +1,22 @@
 var slaying = true;
-// Um pouco de mágica matemática nova para calcular as chances
-// de atingir o dragão. Vamos falar disso logo!
+// Um pouco de mÃ¡gica matemÃ¡tica nova para calcular as chances
 var youHit = Math.floor(Math.random() * 2);
 var damageThisRound = Math.floor(Math.random() * 5 + 1);
 var totalDamage = 0;
 
 while (slaying) {
   if (youHit) {
-    console.log("Você atingiu o dragão e causou " + damageThisRound + " de dano!");
+    console.log("VocÃª atingiu o dragÃ£o e causou " + damageThisRound + " de dano!");
     totalDamage += damageThisRound;
     
     if (totalDamage >= 4) {
-      console.log("Você conseguiu! Voce matou o dragao!");
+      console.log("VocÃª conseguiu! Voce matou o dragao!");
       slaying = false;
     } else {
       youHit = Math.floor(Math.random() * 2);
     }
   } else {
-    console.log("Você foi derrotado pelo dragão! Ficou torrado.");
+    console.log("VocÃª foi derrotado pelo dragÃ£o! Ficou torrado.");
     slaying = false;
   }
 }
